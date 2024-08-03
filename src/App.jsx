1707f8cae8872e7042d2e102c016ed665491d2d9
codeBelal <coderbelal99@gmail.com>
2024-08-03 11:32:39 +0600

    Updated App.jsx and added .gitattributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Card from './Components/Card/Card'
+import MainCard from './Components/MainCard/MainCard'
 const App = () => {
   return (
     <div>
       <Card/>
+      <MainCard/>
     </div>
   )
 }
---
+++ src/Components/MainCard/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainCard = () => {
+  return (
+    <div>
+      <h1 className=' bg-red-500  w-20 rounded-lg'>This is Card Components</h1>
+    </div>
+  )
+}
+
+export default MainCard
